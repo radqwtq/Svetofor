3031bc2a1ad51ffe1d966d101f36219f87ae4d7d
JirkovaD <jirkovad@iMac6--Administrator.local>
2023-03-29 11:15:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/svetofor/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.svetofor
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import java.util.Timer
+import java.util.TimerTask
+
+class MainActivity : Activity() {
+    var imSemafor: ImageView? = null
+    var counter: Int = 0
+    var timer: Timer? = null
+    var is_run = false
+    var imageArray: IntArray = intArrayOf(R.drawable.semafor_red, R.drawable.semafor_green, R.drawable.semafor_yellow)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imSemafor = findViewById(R.id.imSemafor)
+        //imSemafor?.setImageResource(R.drawable.semafor_green)
+        imSemafor?.setImageResource(imageArray[1])
+    }
+
+    fun onClickStartStop(view: View) {
+        view as ImageButton
+        if (!is_run) {
+            startStop()
+            timer = Timer()
+            view.setImageResource(R.drawable.button_stop)
+            is_run = true
+        }
+        else
+        {
+    imSemafor?.setImageResource(R.drawable.semafor_green)
+    view.setImageResource(R.drawable.button_start)
+     timer?.cancel()
+     is_run = false
+     counter = 0
+    }
+
+}
+
+    fun startStop(){
+    timer = Timer()
+    timer?.schedule(object : TimerTask() {
+            override fun run () {
+                imSemafor?.setImageResource(imageArray[counter])
+                counter++
+                if (counter == 3) counter = 0
+            }
+
+    } ,0,1000)
+    }
+}
